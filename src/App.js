66a7a3b84633fdddedd989a34f69e215b690cb4b
jaeseokking <wotjr294@naver.com>
2022-02-02 20:41:15 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Button from './Button';
-import styles from './App.module.css'
+import {useState, useEffect} from 'react';
+
+function Hello(){
+  function byFn(){
+    console.log("destroyed :(");
+  }
+  function hiFn(){
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn ,[])
+  return <h1>Hello</h1>
+}
+
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev)
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "Hide" :  "Show" }</button>
     </div>
   );
 }
